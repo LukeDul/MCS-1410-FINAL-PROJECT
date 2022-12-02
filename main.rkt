ab9174e4f73c310dab7c778b3c9d6d962e418405
Luke Dulworth <lukedulworth2@gmail.com>
2022-12-02 15:22:54 -0600

    basic collsion

--- main.rkt
+++ main.rkt
@@ -1,11 +1,14 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-advanced-reader.ss" "lang")((modname main) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #t #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
-; the width of the window 
+; the width of the game window 
 (define WINDOW-WIDTH 500)
 
-; the height of the window 
+; the height of the game window 
 (define WINDOW-HEIGHT 500)
+
+; size of the player's hitbox 
+(define PLAYER-HITBOX 7)
 
 ; player contains the heading, position & speed of the player character.
 (define-struct player [heading position speed])
@@ -13,7 +16,15 @@
 ; holds the state of each usuable key 
 (define-struct keys [w a s d])
 
-(define-struct world-state [player keyboard])
+; contains the width, height, and position of a hitbox 
+(define-struct hit-box [width height x y])
+
+; worldstate 
+(define-struct world-state [player keyboard hit-box])
+
+
+
+;******************************************************* keyboard handling *******************************************************
 
 
 ; World State, Key -> World State
@@ -43,14 +54,18 @@
          [(key=? input-key "a") (update-keys struct(make-keys w #f s d))]
          [(key=? input-key "s") (update-keys struct(make-keys w a #f d))]   
          [(key=? input-key "d") (update-keys struct(make-keys w a s #f))]
-         [else struct])))
+         [else struct]))) 
 
 
 ; World State, Keys -> World State 
-(define (update-keys struct new-keys) (make-world-state (world-state-player struct) new-keys))
+(define (update-keys struct new-keys) (make-world-state (world-state-player struct) new-keys (world-state-hit-box struct)))
 
 
 ; (make-player (make-world-state (make-player "NORTH" (make-posn 250 450) 4) (make-keys #false #false #false #false)) (make-posn 250 446) 4)
+
+
+;******************************************************* on-tick / movement *******************************************************
+
 
 ; World State -> World State 
 ; updates heading and position of the player 
@@ -58,10 +73,18 @@
  (local [(define heading (player-heading (world-state-player struct)))
          (define new-position (change-position (world-state-player struct)))
          (define new-heading (heading-handler struct))
-         (define speed (player-speed (world-state-player struct)))]
+         (define speed (player-speed (world-state-player struct)))
+         (define current-position (player-position (world-state-player struct)))]
    
-   (make-world-state (make-player new-heading new-position speed)
-                     (world-state-keyboard struct))))
+   (cond [(square-collision? current-position PLAYER-HITBOX (world-state-hit-box struct)); if player is colliding w/ hitbox
+          (make-world-state (make-player heading current-position speed) ; freeze player 
+                            (world-state-keyboard struct)
+                            (world-state-hit-box struct))] 
+         
+         [else (make-world-state (make-player new-heading new-position speed) ; updates player
+                                 (world-state-keyboard struct)
+                                 (world-state-hit-box struct))]))) ; retains keyboard
+                                 
 
 
 ; World State -> String
@@ -81,7 +104,7 @@
           [a "WEST"]
           [s "SOUTH"]
           [d "EAST"]
-          [else heading])))
+          [else heading])))  
 
 
 ; Player Structure -> Posn
@@ -111,41 +134,83 @@
 (define (percentage percent number)
   (* (/ percent 10) (/ number 10)))
 
-  
+
+;**************************************************** collision ****************************************************
+
+
+; Posn, Hit-box -> Boolean
+; checks if a 4 points surrounding a point is colliding with a point 
+(define (square-collision? point size hit-box)
+  (if (or
+       (point-collision? (make-posn (+ size (posn-x point)) (posn-y point)) hit-box) ; left 
+       (point-collision? (make-posn (- (posn-x point) size) (posn-y point)) hit-box) ; right
+       (point-collision? (make-posn (posn-x point) (+ size (posn-y point))) hit-box) ; top 
+       (point-collision? (make-posn (posn-x point) (- (posn-y point) size)) hit-box) ; bottom 
+
+       (point-collision? (make-posn (+ (posn-x point) size) (+ (posn-y point) size)) hit-box) ; top right 
+       (point-collision? (make-posn (- (posn-x point) size) (+ (posn-y point) size)) hit-box) ; top left 
+       (point-collision? (make-posn (- (posn-x point) size) (- (posn-y point) size)) hit-box) ; bottom left 
+       (point-collision? (make-posn (+ (posn-x point) size) (- (posn-y point) size)) hit-box) ; bottom right 
+       )
+      #t
+      #f)) 
+
+
+; Posn, hit-box, -> Boolean
+; Given a point and a rectangular hit-box returns whether the point the hitbox contains the point
+(define (point-collision? point hit-box)
+  (if (and
+       (<= (posn-x point) (+ (hit-box-x hit-box) (/ (hit-box-width hit-box) 2)))
+       (>= (posn-x point) (- (hit-box-x hit-box) (/ (hit-box-width hit-box) 2)))
+       (<= (posn-y point) (+ (hit-box-y hit-box) (/ (hit-box-height hit-box) 2)))
+       (>= (posn-y point) (- (hit-box-y hit-box) (/ (hit-box-height hit-box) 2))))
+      #t
+      #f)) 
+
+
+;******************************************************* rendering ****************************************************
+ 
 ; World State -> Image
 ; Given the Player Structure, struct, returns an image of the playerable character at the Player Structure's coordinates. 
 (define (draw struct)
   (local [(define x (posn-x (player-position (world-state-player struct))))
           (define y (posn-y (player-position (world-state-player struct))))
           (define heading (player-heading (world-state-player struct)))
-          (define player-image (circle 10 'solid 'red))
+          (define player-image (square (* 2 PLAYER-HITBOX) 'solid 'red))  ; (* PLAYER-HITBOX (sqrt 2))
           (define background (rectangle WINDOW-WIDTH WINDOW-HEIGHT 'solid 'gray))
           (define w (keys-w (world-state-keyboard struct)))
           (define a (keys-a (world-state-keyboard struct)))
           (define s (keys-s (world-state-keyboard struct)))
           (define d (keys-d (world-state-keyboard struct)))]
     
-    (place-image (above (text (if w "#t" "#f") 12 "olive")
+    (place-image (above (text (if w "#t" "#f") 12 "olive") 
                         (text (if a "#t" "#f") 12 "olive")
                         (text (if s "#t" "#f") 12 "olive")
-                        (text (if d "#t" "#f") 12 "olive")
-                        (text heading 12 "olive"))
+                        (text (if d "#t" "#f") 12 "olive") 
+                        (text heading 12 "olive")) 
                  100 100
-                 (place-image player-image x y background))))
+                 (place-image player-image x y (overlay (square 10 'solid 'black) background))))) 
 
+
+;******************************************************* initial states ************************************************
 
 ; the initial state of the playable character. 
 (define initial-player (make-player "NORTH"
                                     (make-posn (/ WINDOW-WIDTH 2) ; aligns player on the middle of x-axis
                                                (ceiling (percentage 90 WINDOW-HEIGHT))) ; aligns player on 90% of y-axis
-                                    8)) ; initial speed
+                                    1)) ; initial speed
+
+(define test-dummy (make-hit-box 10 10 (/ WINDOW-HEIGHT 2) (/ WINDOW-WIDTH 2)))
  
 (define initial-keys (make-keys #f #f #f #f)) 
 
-(define initial-world-state (make-world-state initial-player initial-keys))  
+(define initial-world-state (make-world-state initial-player initial-keys test-dummy)) 
+
+
+;******************************************************* big bang ***********************************************
 
 (big-bang initial-world-state 
-  (on-tick tock)  
+  (on-tick tock 0.001)  
   (to-draw draw)
   (state #f)
   (on-key press-handler)
